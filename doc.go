610b7723de8c804465db81cd203dedf6a9bc7f6e
Brady Catherman <github@gecka.us>
2014-09-11 22:49:01 -0600

    Fix the section titles in doc.go

--- doc.go
+++ doc.go
@@ -17,7 +17,7 @@
 // functionality is reduced into helpful functions that require little to no
 // additional checking or setup.
 //
-// Use Case 1: Full Stack Traces
+// Full Stack Traces
 //
 // Sometimes it is helpful to write a wrapper function to do a bunch of the
 // boiler plate testing for you, however this causes the testing library
@@ -25,7 +25,7 @@
 // library implements a Fatal/Fatalf that will output the full stack
 // trace where the error was encountered.
 //
-// Use Case 2: Easier Error Checking
+// Easier Error Checking
 //
 // Often error checking around initialization or setup functionality can become
 // extensive which means that often error returns will get ignored since
@@ -34,7 +34,7 @@
 // This library makes testing for expected errors or success super trivial.
 // See the Examples section for simple examples explaining how to do this.
 //
-// Use Case 3: Temporary Files are Not
+// Temporary Files are Not
 //
 // When writing temporary files people often assume that something will
 // clean them up for them but in Go this is far from the truth. Often an
@@ -48,7 +48,7 @@
 // created. Using a new processes helps ensure that a panic or hard crash
 // won't prevent files from being cleaned.
 //
-// Use Case 4: Simple Equality
+// Simple Equality
 //
 // It can be quite tedious to validate that two objects are equal. Especially
 // if they are maps of complex data types. Often this leads to dozens of
